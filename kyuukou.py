4bc2e3fc2d551a85d1c858be45be7a1056e86c91
baki <yohtsubaki@yahoo.co.jp>
2021-03-02 16:32:13 +0900

    fix text

--- kyuukou.py
+++ kyuukou.py
@@ -1,7 +1,7 @@
-#!pip3 install requests
-#!pip3 install beautifulsoup4
-#!pip3 install chardet
-#!pip3 install schedule
+!pip3 install requests
+!pip3 install beautifulsoup4
+!pip3 install chardet
+!pip3 install schedule
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,21 +13,32 @@ prev = 'a'
 def dailyjob():
   site = requests.get('http://kyoumu.office.uec.ac.jp/kyuukou/kyuukou.html')
   soup = BeautifulSoup(site.content,'html.parser')    #Countermeasures against garbled text
-  caninfo = soup.find('p').text   #information of cancel  
-  #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はcaninfo#########
+  Allcaninfo = soup.find('p').text   #information of cancel
+  Splitcaninfo = Allcaninfo.split("\n")
+  for caninfo in Splitcaninfo:
+    if "現在、休講の予定はありません。" in caninfo :
+      #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はcaninfo#########
+    elif "月" in caninfo:
+      #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はAllcaninfo#########
+      break
 
 def minjob():
-  ite = requests.get('http://kyoumu.office.uec.ac.jp/kyuukou/kyuukou.html')
+  site = requests.get('http://kyoumu.office.uec.ac.jp/kyuukou/kyuukou.html')
   soup = BeautifulSoup(site.content,'html.parser')    #Countermeasures against garbled text
-  caninfo = soup.find('p').text   #information of cancel 
+  Allcaninfo = soup.find('p').text   #information of cancel
   
-  if prev != caninfo:
-    #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はcaninfo#########
-    prev = caninfo
+  if prev != Allcaninfo:
+      Splitcaninfo = Allcaninfo.split("\n")
+    for caninfo in Splitcaninfo:
+      if "現在、休講の予定はありません。" in caninfo :
+        #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はcaninfo#########
+      elif "月" in caninfo:
+        #########ここにbotがチャンネルに書き込むためのプログラムを書く。変数はAllcaninfo#########
+        break
 
 def main():
   #Everyday
   schedule.every().day.at("20:00").do(dailyjob)
 
   #Every10min
-  schedule.every(1).second.do(minjob)
+  schedule.every(10).minutes.do(minjob)
